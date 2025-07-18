97bdfe5ac72022dfbe81ed571a644bb4e304ae86
ARTMARVL <vladyslav.anishchenko@gmail.com>
2025-07-18 12:15:39 +0200

    1

---
+++ lib/api.tsx
@@ -0,0 +1,65 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import axios from 'axios';
+import { Note } from '@/types/note';
+
+const BASE_URL = 'https://notehub-public.goit.study/api';
+const TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+interface FetchParams {
+  search?: string;
+  page?: number;
+}
+interface NotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+export const fetchNotes = async (
+  page = 1,
+  perPage = 10,
+  search = ''
+): Promise<NotesResponse> => {
+  try {
+    const params: FetchParams = {
+      ...(search.trim() !== '' && { search: search.trim() }),
+      page,
+    };
+    const res = await axiosInstance.get<NotesResponse>('/notes', { params });
+    return res.data;
+  } catch (error) {
+    console.error('Failed to fetch notes:', error);
+    throw new Error('Could not fetch notes. Please try again later.');
+  }
+};
+
+export const fetchNoteById = async (id: number): Promise<Note> => {
+  const { data } = await axiosInstance.get<Note>(`/notes/${id}`);
+  return data;
+};
+
+interface CreateNotePayload {
+  title: string;
+  content: string;
+  tag: string;
+}
+
+export const createNote = async (note: CreateNotePayload): Promise<Note> => {
+  const { data } = await axiosInstance.post<Note>('/notes', note);
+  return data;
+};
+
+export const deleteNote = async (id: string): Promise<Note> => {
+  const { data } = await axiosInstance.delete<Note>(`/notes/${id}`);
+  return data;
+};
